52060e7ae9b5643363e1f1496ce10750b9ba14b3
roccofab <fabizrocco001@gmail.com>
2025-07-30 12:51:29 +0200

    changes for render deploy

--- App/Dashboard.py
+++ App/Dashboard.py
@@ -368,4 +368,5 @@ def update_availability_graph(selected_type):
 server = app.server
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=8050)
+    port = int(os.environ.get('PORT', 8050))
+    app.run(debug=False, host='0.0.0.0', port=port)
