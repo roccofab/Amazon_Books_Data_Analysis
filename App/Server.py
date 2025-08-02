00a7583a9cdd9ff4a43cc2ff605cf64d4ade5287
roccofab <fabizrocco001@gmail.com>
2025-08-02 15:56:21 +0200

    moved Server.py to App directory

--- App/Server.py
+++ App/Server.py
@@ -1,4 +1,6 @@
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from src.Analysis.Reccomandation_Models import kmeans_reccomender, data_preprocessing
